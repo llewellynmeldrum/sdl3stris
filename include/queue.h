f4a8da62045fbbf1a95b07068fbe67df9f1b0d5a
llewellynmeldrum <llewellyn.meldrum1@students.mq.edu.au>
2026-03-18 19:05:23 +1100

    implemented fifo queue as doubly linked list

---
+++ include/queue.h
@@ -0,0 +1,45 @@
+#pragma once
+#include <stddef.h>
+#include <stdio.h>
+#include <unistd.h>
+typedef struct QueueNode {
+    struct QueueNode* next;
+    struct QueueNode* prev;
+    void*             data;
+} QueueNode;
+
+typedef struct Queue {
+    QueueNode*   head;
+    QueueNode*   tail;
+    size_t       size;
+    const size_t element_size;
+} Queue;
+
+static inline void debug_print_q(Queue* q) {
+    QueueNode* cur = q->tail;
+    printf("[");
+    while (cur) {
+        printf("%p->", cur->data);
+        cur = cur->next;
+        printf(", ");
+    }
+    printf("]\n");
+    printf("Queue located @%p:\n", q);
+    printf("Queue{\n"
+           "    QueueNode*    front = %p;\n"
+           "    QueueNode*     back = %p;\n"
+           "    size_t         size = %zu;\n"
+           "    size_t element_size = %zu;\n"
+           "}\n",
+           q->head, q->tail, q->size, q->element_size);
+}
+Queue q_create(const size_t element_size);
+void  q_destroy(Queue* q);
+
+// peeks from front of queue (FIFO)
+void* q_peek(Queue* q);
+// pops from front of queue (FIFO)
+void* q_pop(Queue* q);
+// pushes to back of queue
+void q_push(Queue* q, void* element);
+void q_print(Queue* q, void (*print_function)(void*));
---
+++ src/queue.c
@@ -0,0 +1,75 @@
+#include "queue.h"
+#include "logger.h"
+#include <assert.h>
+#include <stddef.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+
+Queue q_create(const size_t element_size) {
+    return (Queue){
+        .size = 0,
+        .element_size = element_size,
+        .head = NULL,
+        .tail = NULL,
+    };
+}
+void q_print(Queue* q, void (*print_function)(void*)) {
+    QueueNode* cur = q->tail;
+    printf("[");
+    while (cur) {
+        print_function(cur->data);
+        cur = cur->next;
+        printf(", ");
+    }
+    printf("]\n");
+}
+
+void* q_peek(Queue* q) {
+    return q->head;
+}
+
+void* q_pop(Queue* q) {
+    if (q->size == 0 || !q->head) {
+        LOGERR("Tried to pop from empty queue @%p", q);
+        return NULL;
+    }
+    QueueNode* victim = q->head;
+    if (q->size > 1) {
+        QueueNode* new_head = q->head->prev;
+        q->head = new_head;
+        q->head->next = NULL;
+    } else {  // size of 1 means we need to nuke both
+        q->tail = NULL;
+        q->head = NULL;
+    }
+    q->size--;
+    void* res = victim->data;
+    free(victim);
+    return res;
+}
+
+void q_push(Queue* q, void* src) {
+    QueueNode* newtail = calloc(1, sizeof(*newtail));
+    if (!newtail) {
+        LOGERR("Calloc returned null.");
+    }
+    newtail->data = src;
+    QueueNode* oldtail = q->tail;
+    q->tail = newtail;
+    if (oldtail) {
+        newtail->next = oldtail;
+        oldtail->prev = newtail;
+    }
+    if (q->size == 0) {
+        q->head = q->tail;
+    }
+    q->size++;
+}
+
+void q_destroy(Queue* q) {
+    while (q->size > 0) {
+        q_pop(q);
+    }
+}
